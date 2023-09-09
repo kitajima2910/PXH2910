2f1d0a2812713bec2075112b046607e5dd969695
kitajima2910 <kitajima2910@gmail.com>
2023-09-09 10:56:39 +0700

    - fix code index.html

---
+++ assets/js/datas.js
@@ -0,0 +1,17 @@
+// HTML CSS JS
+export const DATA_HTML_CSS_JS = [
+    {
+        tooltip: "Sử dụng phím Space, X, Arrow Up để nhảy",
+        img: "assets/img/games/Flappy-Bird.PNG",
+        subTitle: "Flappy Bird",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Flappy-Bird",
+        live: "games/Flappy-Bird/index.html"
+    },
+    {
+        tooltip: "Sử dụng chuột để chơi",
+        img: "assets/img/games/Wack-a-Mole.PNG",
+        subTitle: "Trò Chơi Đập Chuột",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Whack-a-Mole",
+        live: "games/Whack-a-Mole/index.html"
+    }
+];
