1a1328d8ae3cc1213a9757ffc7e778fee42e49b9
kitajima2910 <kitajima2910@gmail.com>
2023-09-30 19:30:28 +0700

    - Kanban w/ Draggable Cards in Angular :: Drag and Drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './kanban/pages/main-view/main-view.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { MainViewComponent } from './kanban/pages/main-view/main-view.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
