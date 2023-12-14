b8d201e7d6a7e4cd368b2e2f7206f2944ab8d8b5
kitajima2910 <kitajima2910@gmail.com>
2023-12-14 19:56:25 +0700

    - Game Drag And Drop

---
+++ backup_pxh2910_pure/games/Drag-And-Drop/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1702555563,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/keo01-sheet0.png",
+		"images/keo02-sheet0.png",
+		"images/keo03-sheet0.png",
+		"images/keo04-sheet0.png",
+		"images/keo05-sheet0.png",
+		"images/khoi01-sheet0.png",
+		"images/khoi02-sheet0.png",
+		"images/khoi03-sheet0.png",
+		"images/khoi04-sheet0.png",
+		"images/khoi05-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/thuoctg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
