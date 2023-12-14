230058485c76ef84ca123b987872c10b2b6415f1
kitajima2910 <kitajima2910@gmail.com>
2023-12-14 20:18:36 +0700

    - Delete path backup_pxh2910_pure

--- components/games/htmlcssjs/js/data.js
+++ components/games/htmlcssjs/js/data.js
@@ -3,42 +3,42 @@ var DATA_HTML_CSS_JS = [
         tooltip: "Sử dụng chuột để kéo thả vào hộp cùng màu",
         img: "assets/img/games/Drag-And-Drop.PNG",
         subTitle: "Kéo Thả Vào Hộp",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Drag-And-Drop",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Drag-And-Drop",
         live: "games/Drag-And-Drop/index.html"
     },
     {
         tooltip: "Arrow(Up, Down, Left, Right) để di chuyển",
         img: "assets/img/games/Snake.PNG",
         subTitle: "Rắn Săn Mồi",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Snake",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Snake",
         live: "games/Snake/index.html"
     },
     {
         tooltip: "Sử dụng chuột để kéo thả Candy",
         img: "assets/img/games/Candy-Crush.PNG",
         subTitle: "Candy Crush",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Candy-Crush",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Candy-Crush",
         live: "games/Candy-Crush/index.html"
     },
     {
         tooltip: "A,D hoặc Arrow(Left, Right) để di chuyển qua lại, Space để chơi lại",
         img: "assets/img/games/Doodler-Jump.PNG",
         subTitle: "Doodle Nhảy",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Doodle-Jump",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Doodle-Jump",
         live: "games/Doodle-Jump/index.html"
     },
     {
         tooltip: "Space, X, Arrow Up để nhảy",
         img: "assets/img/games/Flappy-Bird.PNG",
         subTitle: "Flappy Bird",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Flappy-Bird",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Flappy-Bird",
         live: "games/Flappy-Bird/index.html"
     },
     {
         tooltip: "Dùng Chuột nhấn",
         img: "assets/img/games/Wack-a-Mole.PNG",
         subTitle: "Trò Chơi Đập Chuột",
-        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/backup_pxh2910_pure/games/Whack-a-Mole",
+        githubRepo: "https://github.com/kitajima2910/PXH2910/tree/master/games/Whack-a-Mole",
         live: "games/Whack-a-Mole/index.html"
     }
 ];
