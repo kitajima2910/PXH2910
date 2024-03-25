e9d278ded984986561a6306f443c50aaf439928d
Phạm Xuân Hoài <50172777+kitajima2910@users.noreply.github.com>
2024-03-25 16:42:15 +0700

    fix code js bounding box check

--- games/Motion-sensors/js/main.js
+++ games/Motion-sensors/js/main.js
@@ -1,12 +1,30 @@
 const ball = document.querySelector(".ball")
+const wall = document.querySelector(".wall")
 const move = 10
 
 const handleMotion = e => {
-    const x = e.accelerationIncludingGravity.x * move
-    const y = e.accelerationIncludingGravity.y * move
-    const z = e.accelerationIncludingGravity.z * move
+    let x = e.accelerationIncludingGravity.x * move
+    let y = e.accelerationIncludingGravity.y * move
+    let z = e.accelerationIncludingGravity.z * move
 
-	document.querySelector("#x").value = "alpha X: " + x;
+    // Bounding Box Check
+    if (x < 0) {
+        x = 0
+    }
+
+    if (y < 0) {
+        y = 0
+    }
+
+    if (x > wall.clientWidth) {
+        x = wall.clientWidth
+    }
+
+    if (y > wall.clientHeight) {
+        y = wall.clientHeight
+    }
+
+    document.querySelector("#x").value = "alpha X: " + x;
 	document.querySelector("#y").value = "alpha Y: " + y;
 	document.querySelector("#z").value = "alpha z: " + z;
 
