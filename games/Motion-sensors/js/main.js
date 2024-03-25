f3cee557842efc91ba226b9b4c527d360d6bf13e
Phạm Xuân Hoài <50172777+kitajima2910@users.noreply.github.com>
2024-03-25 14:39:37 +0700

    fix css

--- games/Motion-sensors/js/main.js
+++ games/Motion-sensors/js/main.js
@@ -1,14 +1,14 @@
 const ball = document.querySelector(".ball")
-const move = 10
+const move = 5
 
 const handleMotion = e => {
     const x = Math.round(e.accelerationIncludingGravity.x) * move
     const y = Math.round(e.accelerationIncludingGravity.y) * move
     const z = Math.round(e.accelerationIncludingGravity.z) * move
 
-	document.querySelector("#x").value = x;
-	document.querySelector("#y").value = y;
-	document.querySelector("#z").value = z;
+	document.querySelector("#x").value = "alpha X: " + x;
+	document.querySelector("#y").value = "alpha Y: " + y;
+	document.querySelector("#z").value = "alpha z: " + z;
 
     ball.style.transform = `translate3d(${-x}px, ${y}px, ${z}px)`
 
