b5f9f7ec0700dc90c813611c787a714316a90ff8
Phạm Xuân Hoài <50172777+kitajima2910@users.noreply.github.com>
2024-03-25 16:48:40 +0700

    fix code js

--- games/Motion-sensors/js/main.js
+++ games/Motion-sensors/js/main.js
@@ -7,10 +7,6 @@ const handleMotion = e => {
     let y = e.accelerationIncludingGravity.y * move
     let z = e.accelerationIncludingGravity.z * move
 
-    // Bounding Box Check
-    console.log("pxh wall.clientWidth: ", wall.clientWidth)
-    console.log("pxh wall.clientHeight: ", wall.clientHeight)
-
     document.querySelector("#x").value = "alpha X: " + x;
 	document.querySelector("#y").value = "alpha Y: " + y;
 	document.querySelector("#z").value = "alpha z: " + z;
