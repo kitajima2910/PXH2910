46603a951354b511568e310ace86889b5958e05a
kitajima2910 <kitajima2910@gmail.com>
2024-08-17 21:03:32 +0700

    - Add virtual numbers

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -10,6 +10,13 @@ const Menu = () => {
             <div className="logo">
                 <Link to="/">
                     <img src={logo} alt="Logo PXH2910" />
+                    <div className="virtual-numbers">
+                        <picture>
+                            <img src="https://camo.githubusercontent.com/ebc5f79440b014691fb554090298eba5bbcce45f4b613db6c8658b411c4ddb4f/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f6b6974616a696d61323931302f636f756e742e7376673f"
+                                data-canonical-src="https://profile-counter.glitch.me/kitajima2910/count.svg?"
+                                alt="Virtual Numbers" />
+                        </picture>
+                    </div>
                 </Link>
             </div>
             <div className="name">
