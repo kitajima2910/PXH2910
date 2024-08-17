5992b54d5b92203c2deeebf8090abaf3329d73b9
kitajima2910 <kitajima2910@gmail.com>
2024-08-17 22:48:56 +0700

    - Add games + about

---
+++ src/components/About/index.jsx
@@ -0,0 +1,21 @@
+import "./assets/main.css"
+
+const About = () => {
+
+    return (
+
+        <div className="About">
+            <h2>GIỚI THIỆU</h2>
+            <p>
+                <span>Chào mừng bạn, ghé thăm website của mình!</span>
+                <span>Ở đây chứa các "project mini" luyện tập của mình. Trong các danh sách bên dưới, gồm có "demo" và "source github repo (contact facebook)" để tải về vọc *_^, trong tương lai sẽ còn nhiều hơn nữa!</span>
+                <span>Ngoài ra, sẽ có các chủ đề khác hay một số "projects" khác về sau.</span>
+            </p>
+            <p>Cập nhật: 2023-09-08</p>
+        </div>
+
+    )
+
+}
+
+export default About
--- src/widgets/ImagesGame/index.jsx
+++ src/widgets/ImagesGame/index.jsx
@@ -2,6 +2,7 @@ import "./assets/main.css"
 import { DATA_HTML_CSS_JS } from "../../common/DataCommon"
 import parse from "html-react-parser"
 import { Link } from "react-router-dom"
+import About from "../../components/About"
 
 const datas = DATA_HTML_CSS_JS
 
@@ -11,31 +12,35 @@ const ImagesGame = () => {
 
     return (
 
-        <div className="ImagesGame">
+        <>
+            <About />
 
-            {datas && datas.map((data) =>
-            (
-                <div key={data.title} className="box">
-                    <div className="sub-box">
-                        <img src={data.img} alt="Logo Game" />
-                        <Link to={"/Games"} state={{ dataGameplay: data.live, title: data.title }}>
-                            <div className="overlayer-v1">
-                                <div className="overlayer-v2">
-                                    <div className="btn-playgame">Play Game</div>
-                                    <div className="tutorial">
-                                        {parse(data.tutorial)}
+            <div className="ImagesGame">
+
+                {datas && datas.map((data) =>
+                (
+                    <div key={data.title} className="box">
+                        <div className="sub-box">
+                            <img src={data.img} alt="Logo Game" />
+                            <Link to={"/Games"} state={{ dataGameplay: data.live, title: data.title }}>
+                                <div className="overlayer-v1">
+                                    <div className="overlayer-v2">
+                                        <div className="btn-playgame">Play Game</div>
+                                        <div className="tutorial">
+                                            {parse(data.tutorial)}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
+                        <div className="title">{data.title}</div>
+                        <div className="platform">{data.platform}</div>
                     </div>
-                    <div className="title">{data.title}</div>
-                    <div className="platform">{data.platform}</div>
-                </div>
-            )
-            )}
+                )
+                )}
 
-        </div>
+            </div>
+        </>
 
     )
 
