1cf5d1bea4bf7167f106f32d315b531049684d1e
kitajima2910 <kitajima2910@gmail.com>
2024-11-28 22:10:25 +0700

    - Add game avoid falling blocks

---
+++ public/assets/html/Avoid-Falling-Blocks/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1732806039,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/background-sheet2.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/hudbackground-sheet0.png",
+		"images/btnhome-sheet0.png",
+		"images/btnplayagain-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/gogopobg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
