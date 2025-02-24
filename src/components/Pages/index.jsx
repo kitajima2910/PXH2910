45ed92b3dc063d679baf2512417c9f3cd3145d8f
kitajima2910 <kitajima2910@gmail.com>
2025-02-24 13:26:57 +0700

    - getRandomColor

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -13,12 +13,29 @@ const Pages = () => {
         } else {
             document.title = "Pages . PXH2910"
         }
+
+        const links = document.querySelectorAll(".Pages .list-pages-demo a");
+        links.forEach(link => {
+            const color = getRandomColor();
+            link.style.color = color;
+            link.style.border = `2px dashed ${color}`;
+        });
+
     }, [location.pathname, navigate])
+
+    const getRandomColor = () => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        let a = (Math.random() * 0.5 + 0.5).toFixed(2); // Alpha từ 0.5 - 1
+        return `rgba(${r}, ${g}, ${b}, ${a})`;
+    }
 
     return (
         <div className="Pages">
 
             {location.pathname === "/Pages" && <div className="list-pages-demo">
+                <Link to="/assets/html/sub_website/drum_kit/index.html" target="_blank">Drum Kit</Link>
                 <Link to="/assets/html/Portfolio-Art-Kid-Garden/index.html" target="_blank">Portfolio Art Kid Garden</Link>
                 <Link to="/Pages/Gallery">Gallery</Link>
             </div>}
